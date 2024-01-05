6a42687012554a6d9068e520ed128d8d9ea5f37a
ispmdevelop <ispmdevelop@gmail.com>
2024-01-05 12:15:09 -0400

    remove autogenerated js mock data files

--- mock-data/interval/requests.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validInput1 = void 0;
-exports.validInput1 = {
-    startDate: '2023-12-13 23:19:01.000',
-    endDate: '2023-12-13 23:20:03.000',
-    vehicleId: 'spring-3',
-};
